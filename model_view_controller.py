c73903db131b310641101f03a1d01f5c9bbb0cf9
Dev Strange <devstrangeforgit@gmail.com>
2019-04-18 00:47:52 +0900

    Controller 완성

--- model_view_controller.py
+++ model_view_controller.py
@@ -142,6 +142,42 @@ class Controller(object):
         except mvc_exc.ItemNotStored as e:
             self.view.display_missing_item_error(item_name, e)
 
+    def insert_item(self, name, price, quantity):
+        assert price > 0, 'price must be greater than 0'
+        assert quantity >= 0, 'quantity must be greater than or equal to 0'
+        item_type = self.model.item_type
+        try:
+            self.model.create_item(name, price, quantity)
+            self.view.display_item_stored(name, item_type)
+        except mvc_exc.ItemAlreadyStored as e:
+            self.view.display_item_already_stored_error(name, item_type, e)
+
+    def update_item(self, name, price, quantity):
+        assert price > 0, 'price must be greater than 0'
+        assert quantity >= 0, 'quantity must be greater than or equal to 0'
+        item_type = self.model.item_type
+
+        try:
+            older = self.model.read_item(name)
+            self.model.update_item(name, price, quantity)
+            self.view.display_item_updated(
+                name, older['price'], older['quantity'], price, quantity)
+        except mvc_exc.ItemNotStored as e:
+            self.view.display_item_not_yet_stored_error(name, item_type, e)
+
+    def update_item_type(self, new_item_type):
+        old_item_type = self.model.item_type
+        self.model.item_type = new_item_type
+        self.view.display_change_item_type(old_item_type, new_item_type)
+
+    def delete_item(self, name):
+        item_type = self.model.item_type
+        try:
+            self.model.delete_item(name)
+            self.view.display_item_deletion(name)
+        except mvc_exc.ItemNotStored as e:
+            self.view.display_item_not_yet_stored_error(name, item_type, e)
+
 
 def main():
     my_items = [
@@ -155,6 +191,12 @@ def main():
     c.show_items(bullet_points=True)
     c.show_item('chocolate')
     c.show_item('bread')
+    c.insert_item('bread', price=1.0, quantity=5)
+    c.insert_item('chocolate', price=2.0, quantity=10)
+    c.show_item('chocolate')
+    c.update_item('milk', price=1.2, quantity=20)
+    c.update_item('ice cream', price=3.5, quantity=20)
+    c.delete_item('bread')
 
 
 if __name__ == '__main__':
